87044b6cf6769f15fa02b6e35963b5ac8533c021
Nick Horohk <nickhorohk2005@gmail.com>
2020-10-04 20:29:25 +0300

    FieldTest - checks for Field have been added

--- Test/com/project/xo/Model/FieldTest.java
+++ Test/com/project/xo/Model/FieldTest.java
@@ -1,5 +1,7 @@
 package com.project.xo.Model;
 
+import com.project.xo.Model.Exceptions.AlreadyOccupiedException;
+import com.project.xo.Model.Exceptions.InvalidPointException;
 import org.junit.Test;
 
 import java.awt.*;
@@ -16,7 +18,7 @@ public class FieldTest {
     }
 
     @Test
-    public void setFigureTest() {
+    public void setFigureTest() throws AlreadyOccupiedException, InvalidPointException {
         final Field field = new Field();
         final Point inputPoint = new Point(0, 0);
         final Figure inputFigure = Figure.O;
@@ -26,4 +28,70 @@ public class FieldTest {
 
         assertEquals(inputFigure, actualFigure);
     }
+
+    @Test
+    public void getFigureWhenFigureIsNotSetTest() throws InvalidPointException {
+        final Field field = new Field();
+        final Point inputPoint = new Point(0, 0);
+
+        final Figure actualFigure = field.getFigure(inputPoint);
+
+        assertNull(actualFigure);
+    }
+
+    @Test
+    public void getFigureWhenXLessThenZeroTest(){
+        final Field field = new Field();
+        final Point inputPoint = new Point(-1, 0);
+
+        try {
+            field.getFigure(inputPoint);
+            fail();
+        } catch (InvalidPointException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Test
+    public void getFigureWhenXBiggerThanSizeTest(){
+        final Field field = new Field();
+        final Point inputPoint = new Point(field.getSize() + 1, 0);
+
+        try {
+            field.getFigure(inputPoint);
+            fail();
+        } catch (InvalidPointException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Test
+    public void getFigureWhenYLessThenZeroTest(){
+        final Field field = new Field();
+        final Point inputPoint = new Point(0, -1);
+
+        try {
+            field.getFigure(inputPoint);
+            fail();
+        } catch (InvalidPointException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Test
+    public void getFigureWhenYBiggerThanSizeTest(){
+        final Field field = new Field();
+        final Point inputPoint = new Point(0, field.getSize() + 1);
+
+        try {
+            field.getFigure(inputPoint);
+            fail();
+        } catch (InvalidPointException e) {
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * Также нужно написать тесты для оюоих случаев, когда X и Y оба больше Size или менше 0
+     */
 }
